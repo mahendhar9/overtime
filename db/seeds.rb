3c3244840a95d308db815a39bb42148a069a4132
Mahendhar <mahendarreddy@live.com>
2017-02-24 15:52:53 +0530

    Show all posts on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+user = User.create(email: 'test@test.com', password: "test_pass", password_confirmation: 'test_pass', first_name: "Test", last_name: "test")
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: user.id)
 end
