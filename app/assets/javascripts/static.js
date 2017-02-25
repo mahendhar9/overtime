9ae889974eb65100dad68242d98e504f52aa85b6
Mahendhar <mahendarreddy@live.com>
2017-02-26 00:43:04 +0530

    Implement partial for full coverage of flash messages

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$.getScript("https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js").done(function(response, status) {
 		WebFont.load({
 			google: {
-				families: ['Open Sans']
+				families: ['Open Sans', 'Patua One']
 			}
 		});
 	});
