6179f93a80c471fe35c87212a4d93bb487abdd8f
Mahendhar <mahendarreddy@live.com>
2017-02-27 00:41:26 +0530

    Create resource for Audit Log

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
+  has_many :audit_logs, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,7 +12,6 @@ class User < ActiveRecord::Base
   validates_format_of :phone, with: PHONE_REGEX
   validates :phone, length: { is: 10 }
 
-  has_many :posts, dependent: :destroy
 
   def full_name
   	first_name + " " + last_name
