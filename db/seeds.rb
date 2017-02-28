d259ecb279c83699f5dae62893a0de6de2710d7f
Mahendhar <mahendarreddy@live.com>
2017-02-28 17:27:48 +0530

    Show Audit Logs on AuditLog index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ AdminUser.create(email: 'admin@test.com', password: "test_pass", password_confir
 end
 
 100.times do |post|
-	AuditLog.create(user_id: User.last.id, status: 0, start_date: Date.today-6.days)
+	AuditLog.create(user_id: user.id, status: 0, start_date: Date.today-6.days)
 end
---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'AuditLog features' do
+	before do 
+		@admin_user = FactoryGirl.create(:admin_user)
+		login_as(@admin_user, scope: :user)
+		@audit_log = FactoryGirl.create(:audit_log)
+	end
+
+	describe "index" do
+		it "has an index page that can be reached" do
+			visit audit_logs_path
+			expect(page.status_code).to eq(200)
+		end
+
+		it "shows audit logs on the index page" do 
+			visit audit_logs_path
+			expect(page).to have_content(/Test/)
+		end
+	end
+end
