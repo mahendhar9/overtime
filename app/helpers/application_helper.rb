d7febde6585ee8664643f5a2a847c6a9915aa9ab
Mahendhar <mahendarreddy@live.com>
2017-02-28 17:47:15 +0530

    Integrate enum for AuditLog and implement a helper for status labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+
+	def status_label(status)
+		case status
+		when 'submitted'
+			content_tag(:span, status.titleize, class: 'label label-primary')
+		when 'approved'
+			content_tag(:span, status.titleize, class: 'label label-success')
+		when 'rejected'
+			content_tag(:span, status.titleize, class: 'label label-danger')
+		when 'pending'
+			content_tag(:span, status.titleize, class: 'label label-primary')
+		when 'confirmed'
+			content_tag(:span, status.titleize, class: 'label label-success')
+		end
+	end
 end
