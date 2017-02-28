3caf7107628bacf15047ca2bc71993d56d12e436
Mahendhar <mahendarreddy@live.com>
2017-03-01 00:40:11 +0530

    Employee can now confirm the status of audit log from his dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ AdminUser.create(email: 'admin@test.com', password: "test_pass", password_confir
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: user.id, overtime_request: 1.4)
 end
 
-100.times do |post|
-	AuditLog.create(user_id: user.id, status: 0, start_date: Date.today-6.days)
-end
+AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 20.days))
+
