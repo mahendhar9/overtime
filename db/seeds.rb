df30661cf4b41150c64899448d427304568fffd4
Mahendhar <mahendarreddy@live.com>
2017-03-01 17:23:57 +0530

    Implement STI for Employee and rake task for sending SMS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-user = User.create(email: 'test@test.com', password: "test_pass", password_confirmation: 'test_pass', first_name: "Test", last_name: "test", phone: "1234567890")
+employee = Employee.create(email: 'test@test.com', password: "test_pass", password_confirmation: 'test_pass', first_name: "Test", last_name: "test", phone: "1234567890")
 AdminUser.create(email: 'admin@test.com', password: "test_pass", password_confirmation: 'test_pass', first_name: "Admin", last_name: "Name", phone: "1234567890")
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: user.id, overtime_request: 1.4)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: employee.id, overtime_request: 1.4)
 end
 
-AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: employee.id, status: 0, start_date: (Date.today - 20.days))
 
